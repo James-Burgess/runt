97236bc9fce1b18673756fe1f53f83edab56a1de
james-prodigy <james.burgess@prodigyfinance.com>
2020-12-26 16:01:04 +0200

    add do not email me task
    Signed-off-by: james-prodigy <james.burgess@prodigyfinance.com>

---
+++ tasks/do_not_reply/task.py
@@ -0,0 +1,25 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+from email.utils import formataddr
+import re
+from os import getenv
+
+
+def run(email):
+    with open("tasks/do_not_reply/email.html") as html:
+        msg_html = html.read()
+
+    sender = os.getenv("EMAIL_SENDER")
+    msg = MIMEText(msg_html, "html")
+    msg["Subject"] = "You have been blacklisted"
+    msg["From"] = formataddr((str(Header("Do Not Reply", "utf-8")), sender))
+    msg["To"] = email
+
+    server = smtplib.SMTP_SSL("smtp.zoho.com", 465)
+    server.login(os.getenv("EMAIL_USER"), os.getenv("EMAIL_PASS"))
+    print("sending email")
+    server.sendmail(sender,[email], msg.as_string())
+    print("mail sent!")
+    server.quit()
+    return "complete"
